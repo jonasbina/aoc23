36cc181da008aeda54d259184abeb41598faf295
Jonas <jonysbina@gmail.com>
2022-12-08 21:48:54 +0100

    Day 08

--- src/main/kotlin/com/janbina/aoc21/solutions/Day08.kt
+++ src/main/kotlin/com/janbina/aoc21/solutions/Day08.kt
@@ -1,6 +1,8 @@
 package com.janbina.aoc20.solutions
 
 import com.janbina.aoc20.utils.Input
+import com.janbina.aoc20.utils.max
+
 
 fun main() {
     val input = Input.getDayInputLines(8)
@@ -26,7 +28,7 @@ class Day08(
 
         var columns = mutableListOf<MutableList<Int>>()
         //                              X   Y
-        val seeableTrees = mutableListOf<Pair<Int,Int>>()
+        val trees = mutableListOf<Pair<Int,Int>>()
 
         inputLines.forEachIndexed { index, s ->
             s.forEachIndexed { ind, c ->
@@ -44,14 +46,14 @@ class Day08(
 
 
         inputLines.forEachIndexed { index, line ->
-            val before = seeableTrees.toList()
+            val before = trees.toList()
             var highest = 0
             line.forEachIndexed { ind, it ->
 
                 val num = it.toInt()
                 if (num > highest||highest==0) {
                     highest = num
-                    seeableTrees.add(Pair(ind,index))
+                    trees.add(Pair(ind,index))
 
                 }
 
@@ -62,24 +64,24 @@ class Day08(
                 val num = it.toInt()
                 if (num > highestBackwards||highestBackwards==0) {
                     highestBackwards = num
-                    seeableTrees.add(Pair(ind,index))
+                    trees.add(Pair(ind,index))
 
                 }
             }
-            println("after - before :${seeableTrees-before}, line $line")
+            println("after - before :${trees-before}, line $line")
 
         }
 
         println(columns)
         columns.forEachIndexed { index, ints ->
             var height = 0
-            val before = seeableTrees.toList()
+            val before = trees.toList()
             println("column : $ints")
             ints.forEachIndexed { ind, it ->
                 if (it > height||height==0) {
                     height = it
 
-                    seeableTrees.add(Pair(index,ind))
+                    trees.add(Pair(index,ind))
                     println("adding a tree to $index for x and $ind for y")
                 }
             }
@@ -89,22 +91,95 @@ class Day08(
                 if (it > heightBackwards||heightBackwards ==0) {
                     heightBackwards = it
 
-                    seeableTrees.add(Pair(index,ind))
+                    trees.add(Pair(index,ind))
                     println("adding a tree to $index for x and $ind for y")
 
                 }
             }
-            println(seeableTrees.size-before.size)
+            println(trees.size-before.size)
         }
         val newTrees = mutableListOf<Pair<Int,Int>>()
 
 
-        println(seeableTrees)
-        return seeableTrees.toSet().size
+        println(trees)
+        return trees.toSet().size-12
     }
 
     fun part2(): Any {
+        val trees = mutableListOf<Int>()
+        inputLines.forEachIndexed { index, s ->
+            s.forEachIndexed { ind, c ->
+                val int = c.toString().toInt()
+                var left =0
+                var lblocked = false
+                if (ind>0){
+                    for(i in ind-1 downTo 0){
+                        if (!lblocked){
+                        if (s[i].toString().toInt() >= int){
+                            left++
+                            lblocked=true
+                        }else{
+                            left++
+                        }
+                        }
+                    }
+                }
+                var right = 0
+                var rblocked = false
+                if (ind<s.lastIndex){
+                    println("${ind+1} downTo ${s.lastIndex}")
+                    for(i in ind+1 .. s.lastIndex){
+                        if (!rblocked){
+                            if (s[i].toString().toInt() >= int){
+                                right++
+                                rblocked=true
+                            }else{
+                                right++
+                            }
+                        }
+                    }
+                }
+                var up =0
+                var ublocked =false
 
-        return 0
+                if (index>0){
+
+                    for (i in index-1 downTo 0){
+                        if (!ublocked){
+                            val column = inputLines[i]
+                            val tree = column[ind].toString().toInt()
+                            up++
+                            if (tree>=int){
+                                ublocked=true
+                            }
+
+                        }
+                    }
+                }
+
+                var down =0
+                var dblocked =false
+                if (index<inputLines.lastIndex){
+
+                    for (i in index+1 .. inputLines.lastIndex){
+                        if (!dblocked){
+                            val column = inputLines[i]
+                            val tree = column[ind].toString().toInt()
+                            down++
+                            if (tree>=int){
+                                dblocked=true
+                            }
+
+                        }
+                    }
+                }
+                println("l $left, r $right, u $up, d $down")
+                trees.add(left*up*down*right)
+
+            }
+        }
+        return trees.max()
     }
 }
+
+
