b4047e15a52b87a154a3ffd479cbfd4ee91981a0
Jonas <jonysbina@gmail.com>
2022-12-08 12:13:17 +0100

    Day 07

---
+++ src/main/kotlin/com/janbina/aoc21/solutions/Day08.kt
@@ -0,0 +1,67 @@
+package com.janbina.aoc20.solutions
+
+import com.janbina.aoc20.utils.Input
+
+fun main() {
+    val input = Input.getDayInputLines(8)
+    Day08(input).also {
+        println(it.part1())
+        println(it.part2())
+    }
+}
+
+class Day08(
+    private val inputLines: List<String>
+) {
+
+    private val inputInts by lazy { inputLines.map { it.toInt() } }
+    private val inputLinesSplitted by lazy {
+        inputLines.map {
+            it.split(",")
+        }
+    }
+
+    fun part1(): Any {
+        var sum = 0
+        val colums = inputLines.size - 1
+        val columns = mutableListOf<MutableList<Int>>()
+
+        inputLines.forEachIndexed { index, s ->
+            s.forEachIndexed { ind, c ->
+                if (columns.lastIndex >= ind) {
+                    columns[ind].set(index, c.toInt())
+                }
+            }
+        }
+
+        inputLines.forEach { line ->
+            var highest = 0
+            line.forEach {
+
+                val num = it.toInt()
+                if (num > highest) {
+                    highest = num
+                    sum++
+                }
+
+            }
+
+        }
+        columns.forEachIndexed { index, ints ->
+            var height = 0
+            ints.forEach {
+                if (it > height) {
+                    height = it
+                    sum++
+                }
+            }
+        }
+
+        return sum
+    }
+
+    fun part2(): Any {
+
+        return 0
+    }
+}
