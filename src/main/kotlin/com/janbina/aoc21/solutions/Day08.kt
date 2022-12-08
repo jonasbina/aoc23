251929cfd96e731285c1b50d2d01f755feadd6de
Jonas <jonysbina@gmail.com>
2022-12-08 20:19:28 +0100

    Day 08

--- src/main/kotlin/com/janbina/aoc21/solutions/Day08.kt
+++ src/main/kotlin/com/janbina/aoc21/solutions/Day08.kt
@@ -24,68 +24,82 @@ class Day08(
     fun part1(): Any {
 
 
-        val columns = mutableListOf<MutableList<Int>>()
+        var columns = mutableListOf<MutableList<Int>>()
         //                              X   Y
         val seeableTrees = mutableListOf<Pair<Int,Int>>()
 
         inputLines.forEachIndexed { index, s ->
             s.forEachIndexed { ind, c ->
+
                 if (columns.lastIndex >= ind) {
-                    columns[ind].add(index, c.toInt())
+                    columns[ind].add(index, c.toString().toInt())
                 }else{
-                    columns.add(ind, mutableListOf(c.toInt()))
+                    val newList = mutableListOf<Int>()
+
+                    newList.add(index,c.toString().toInt())
+                    columns.add(ind, newList)
                 }
             }
         }
 
+
         inputLines.forEachIndexed { index, line ->
+            val before = seeableTrees.toList()
             var highest = 0
             line.forEachIndexed { ind, it ->
 
                 val num = it.toInt()
-                if (num > highest) {
+                if (num > highest||highest==0) {
                     highest = num
-                    seeableTrees.add(Pair(index,ind))
-                }
-            }
+                    seeableTrees.add(Pair(ind,index))
 
-        }
-        inputLines.forEachIndexed { index, line ->
-            var highest = 0
+                }
+
+            }
+            var highestBackwards = 0
             line.reversed().forEachIndexed { indd, it ->
                 val ind = line.length-1-indd
                 val num = it.toInt()
-                if (num > highest) {
-                    highest = num
-                    seeableTrees.add(Pair(index,ind))
+                if (num > highestBackwards||highestBackwards==0) {
+                    highestBackwards = num
+                    seeableTrees.add(Pair(ind,index))
+
                 }
             }
+            println("after - before :${seeableTrees-before}, line $line")
+
         }
+
         println(columns)
         columns.forEachIndexed { index, ints ->
             var height = 0
+            val before = seeableTrees.toList()
+            println("column : $ints")
             ints.forEachIndexed { ind, it ->
-                if (it > height) {
+                if (it > height||height==0) {
                     height = it
 
                     seeableTrees.add(Pair(index,ind))
                     println("adding a tree to $index for x and $ind for y")
                 }
             }
-        }
-        columns.forEachIndexed { index, ints ->
-            var height = 0
+            var heightBackwards = 0
             ints.reversed().forEachIndexed { indd, it ->
                 val ind = ints.lastIndex-indd
-                if (it > height) {
-                    height = it
+                if (it > heightBackwards||heightBackwards ==0) {
+                    heightBackwards = it
 
                     seeableTrees.add(Pair(index,ind))
                     println("adding a tree to $index for x and $ind for y")
+
                 }
             }
+            println(seeableTrees.size-before.size)
         }
+        val newTrees = mutableListOf<Pair<Int,Int>>()
 
+
+        println(seeableTrees)
         return seeableTrees.toSet().size
     }
 
