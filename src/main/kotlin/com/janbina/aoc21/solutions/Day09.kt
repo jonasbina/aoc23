cc62f660fc465a573415849ddc0b654ac0680d54
Jonas <jonysbina@gmail.com>
2022-12-09 16:36:53 +0100

    Day 09

--- src/main/kotlin/com/janbina/aoc21/solutions/Day09.kt
+++ src/main/kotlin/com/janbina/aoc21/solutions/Day09.kt
@@ -21,113 +21,152 @@ class Day09(
             it.split(",")
         }
     }
-
-    fun part1(): Any {
-        val positions= mutableListOf<Pair<Int,Int>>()
-        val board = listOf("......",
-        "......",
-        "......",
-        "......",
-        "H.....")
+    fun part1():Any{
+        val locations = mutableListOf<Location>()
         var xH = 0
-        var yH = 0
+        var yH=4
         var xT = 0
-        var yT=0
-        board.forEachIndexed { index, s ->
-            if (s.contains('H')){
-                s.forEachIndexed { ind, c ->
-                    if (c=='H'){
-                        xH = ind
-                        xT=ind
-                        yT = index
-                        yH=index
-                    }
+        var yT = yH
+        inputLines.forEach {s->
+            val command = s.split(' ')
+            val direction = command[0]
+            val length = command[1].toInt()
+            for(i in 1..length){
+                var xMove = 0
+                var yMove =0
+                if (direction== "U"){
+                    yH--
                 }
-            }
-        }
-        positions.add(Pair(xT,yT))
-
-
-        inputLines.forEachIndexed { index, line ->
-            val s = line.split(' ')
-            val direction = s[0].toLowerCase()
-            val length =s[1].toInt()
-           for (i in 1..length){
-               println(length)
-            if (direction=="u"){
-                yH--
-            }
-            if (direction=="d"){
-                yH++
-            }
-            if (direction=="l"){
-                xH--
-            }
-            if (direction=="r"){
-                xH++
-            }
-
-            var yAxis = 0
-            var xAxis = 0
-            if (xT!=xH){
-                if (xT>xH){
-                    //Go left
-                    yAxis=xH-xT
-
+                if (direction== "D"){
+                    yH++
                 }
-                if (xT<xH){
-                    //Go Right
-                    yAxis=xH-xT
+                if (direction== "L"){
+                    xH--
                 }
-            }
-
-
-
-
-            if (yT!=yH){
-                if (yT>yH){
-                    //Go Up
-                    yAxis=yH-yT
-
-
+                if (direction== "R"){
+                    xH++
                 }
-                if (yT<yH){
-                    //Go Down
-                    yAxis=yH-yT
-                }
-            }
 
-            if (xAxis.absoluteValue!=1&&yAxis.absoluteValue!=1){
-
-                if (xAxis.absoluteValue>yAxis.absoluteValue){
-
-                    if (xAxis<0){
-                        xT--
-                    }else{
-                        xT++
-                    }
-
+                xMove=xH-xT
+                yMove=yH-yT
+                if (xMove.absoluteValue+yMove.absoluteValue==1){
+                    println("jsem vedle pico")
                 }else{
-                if (yAxis!=0){
-                    if (yAxis<0){
-                        yT--
+                    if (xMove.absoluteValue==1&&yMove.absoluteValue==1){
+                        println("diagonal pico")
                     }else{
-                        yT++
+                        if (xMove>0){
+                            xT++
+                        }
+                        if (yMove>0){
+                            yT++
+                        }
+                        if (xMove<0){
+                            xT--
+                        }
+                        if (yMove<0){
+                            yT--
+                        }
+
+
                     }
-                }}
+                }
+                locations.add(Location(xT,yT))
+            }
+        }
+
+        return locations.toSet().size
+    }
+    fun part2(): Any {
+        val locations = mutableListOf<Location>()
+        var xH = 0
+        var yH=4
+
+        val tails = mutableListOf<Tail>()
+        for (i in 1..9){
+            tails.add(Tail(xH,yH))
+        }
+        locations.add(Location(xH,yH))
+        inputLines.forEach {s->
+            val command = s.split(' ')
+            val direction = command[0]
+            val length = command[1].toInt()
+            for(i in 1..length){
+                var xMove = 0
+                var yMove =0
+                if (direction== "U"){
+                    yH--
+                }
+                if (direction== "D"){
+                    yH++
+                }
+                if (direction== "L"){
+                    xH--
+                }
+                if (direction== "R"){
+                    xH++
+                }
+
+                tails.forEachIndexed { index, tail ->
+
+                    if (index==0){
+                        tail.move(xH,yH)
+                    }else{
+                        val previous = tails[index-1]
+                        tail.move(previous.x,previous.y)
+                    }
+                    locations.add(Location(tail.x,tail.y))
+                }
 
             }
-
-            positions.add(Pair(xT,yT))
+            tails.forEach {
+                println("${it.x}, ${it.y}")
+            }
         }
-        }
 
-
-        return positions.toSet().size
+        return locations.toSet().size
     }
 
-    fun part2(): Any {
 
-        return 0
+}
+data class Location(
+    var x :Int,
+    var y :Int
+)
+class Tail(
+    var x :Int,
+    var y :Int
+) {
+    fun move(xH: Int, yH: Int) {
+        var xT = x
+        var yT = y
+        val xMove = xH - xT
+        val yMove = yH - yT
+        if (xMove.absoluteValue + yMove.absoluteValue == 1) {
+            println("jsem vedle pico")
+        } else {
+            if (xMove.absoluteValue == 1 && yMove.absoluteValue == 1) {
+                println("diagonal pico")
+            } else {
+                if (xMove > 0) {
+                    xT++
+                }
+                if (yMove > 0) {
+                    yT++
+                }
+                if (xMove < 0) {
+                    xT--
+                }
+                if (yMove < 0) {
+                    yT--
+                }
+
+
+            }
+        }
+        x = xT
+        y = yT
     }
 }
+
+
